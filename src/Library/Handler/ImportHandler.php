596a5ffb76137471513d123d5b99f207b6b21d7b
Raistlfiren <avaland@woopta.com>
2021-03-28 08:50:39 -0500

    Refactored handler and put logic into Garmin Helper class.

--- src/Library/Handler/ImportHandler.php
+++ src/Library/Handler/ImportHandler.php
@@ -2,60 +2,8 @@
 
 namespace App\Library\Handler;
 
-use App\Library\Handler\Event\HandlerEvent;
-use App\Library\Handler\Event\HandlerEvents;
-
 class ImportHandler extends AbstractHandler
 {
-    public function handle(HandlerOptions $handlerOptions)
-    {
-        $this->validateFile($handlerOptions);
-//        $workouts = $this->parser->findAllWorkouts();
-//        $event = new HandlerEvent($handlerOptions);
-//        $this->dispatcher->dispatch($event, HandlerEvents::FILE_VALIDATION_STARTED);
-//
-//        $this->parser->isValidFile($handlerOptions->getPath());
-//
-//        $this->dispatcher->dispatch($event, HandlerEvents::FILE_VALIDATION_ENDED);
-
-//        $this->dispatcher->dispatch($event, HandlerEvents::PARSING_WORKOUTS_STARTED);
-//
-//        $period = $this->parser->parse();
-//
-//        $debugMessages = $this->parser->getDebugMessages();
-//        $event->setDebugMessages($debugMessages);
-//        $this->dispatcher->dispatch($event, HandlerEvents::PARSING_WORKOUTS_ENDED);
-
-        $workouts = $this->parseWorkouts($handlerOptions);
-
-//        if ($period === null) {
-//            return;
-//        }
-//
-//        $workouts = $period->getWorkouts();
-//        $days = $period->getDays();
-        $this->overrideClientCredentials($handlerOptions->getEmail(), $handlerOptions->getPassword());
-
-        $this->deleteWorkouts($handlerOptions, $workouts);
-
-//        if ($handlerOptions->getDelete()) {
-//            $this->dispatcher->dispatch($event, HandlerEvents::DELETE_WORKOUTS_STARTED);
-//        }
-//        $debugMessages = $this->deleteWorkouts($workouts, $handlerOptions->getDelete());
-//        $event->setDebugMessages($debugMessages);
-//        $this->dispatcher->dispatch($event, HandlerEvents::DELETE_WORKOUTS_ENDED);
-
-        $this->createWorkouts($handlerOptions, $workouts);
-//        $this->createWorkouts($handlerOptions, $days);
-
-        $this->attachNotes($handlerOptions, $workouts);
-
-//        $event->setDebugMessages($debugMessages);
-//        $this->dispatcher->dispatch($event, HandlerEvents::CREATED_WORKOUTS_ENDED);
-
-        //Attach notes to workouts...
-    }
-
     public function supports(string $command)
     {
         return $command === 'import';
--- src/Library/Parser/Parser.php
+++ src/Library/Parser/Parser.php
@@ -6,17 +6,18 @@ use App\Library\Parser\Model\Day;
 use App\Library\Parser\Model\PeriodCollection;
 use App\Library\Parser\Model\WeekCollection;
 use App\Library\Parser\Model\Workout\WorkoutFactory;
-use League\Csv\Reader;
+use App\Model\DebugMessages;
 use DateTime;
+use League\Csv\Reader;
 
 class Parser
 {
+    use DebugMessages;
+
     /** @var Reader $csv */
     protected $csv;
 
     protected $records;
-
-    protected $debugMessages = [];
 
     /**
      * @param $path
@@ -123,99 +124,6 @@ class Parser
         }
 
         return $period;
-    }
-
-    public function parse(DateTime $startDate = null, $prefix = null)
-    {
-        $period = new PeriodCollection();
-        $debugCounter = 0;
-
-        $days = Day::WEEK;
-        foreach ($this->records as $record) {
-            $week = new WeekCollection();
-            foreach ($days as $day) {
-                $entityDay = new Day();
-                $this->debugMessages[$debugCounter] = '';
-
-                if ($startDate) {
-                    $entityDay->setDate(clone $startDate);
-                    $this->debugMessages[$debugCounter] = $startDate->format('Y-m-d') . ' - ';
-                    //Increment date by 1...
-                    $startDate->modify('+1 day');
-                }
-
-                $week->addDay($entityDay);
-
-                $recordLines = preg_split("/((\r?\n)|(\r\n?))/", $record[$day]);
-                $lineNumbers = count($recordLines);
-                $lineNumber = 0;
-                $workout = "";
-                $workoutArr = array();
-                foreach ($recordLines as $recordLine) {
-                    $lineNumber++;
-                    if (! empty($recordLine)) {
-                        $regex = '/^(' . implode('|',WorkoutTypes::WORKOUTS) . ')?:/';
-                        $result = preg_match($regex, $recordLine, $workoutType);
-
-                        // new workout
-                        if ($result && isset($workoutType[1]) && ! empty($workoutType[1])) {
-                            // push previous workout to array
-                            if ($workout != "") {
-                                array_push($workoutArr, $workout);
-                            }
-                            $workout = $recordLine . "\n";
-                        }
-                        else {
-                            $workout .= $recordLine . "\n";
-                            // last line
-                            if($lineNumber === $lineNumbers) {
-                                array_push($workoutArr, $workout);
-                            }
-                        }
-                    }
-                }
-
-                $workoutNumbers = count($workoutArr);
-                $workoutNumber = 0;
-                foreach ($workoutArr as $workout) {
-                    $workoutNumber++;
-                    $workout = $this->parseWorkout($workout);
-                    $name = $prefix . $workout->getName();
-                    $workout->setName($name);
-                    if ($workoutNumbers > 1) {
-                        $this->debugMessages[$debugCounter] .= $workoutNumber . ". ";
-                    }
-                    $this->debugMessages[$debugCounter] .= (empty($workout->getName()) ? 'Workout parsed.' : $workout->getName());
-                    if ($workoutNumbers > 1 && $workoutNumber < $workoutNumbers) {
-                        $this->debugMessages[$debugCounter] .= "  -  ";
-                    }
-                    $entityDay->addWorkout($workout);
-                    
-                }
-                $debugCounter++;
-            }
-            $period->addWeek($week);
-        }
-
-        return $period;
-    }
-
-    /**
-     * @return array
-     */
-    public function getDebugMessages(): array
-    {
-        return $this->debugMessages;
-    }
-
-    /**
-     * @param array $debugMessages
-     * @return Parser
-     */
-    public function setDebugMessages(array $debugMessages): Parser
-    {
-        $this->debugMessages = $debugMessages;
-        return $this;
     }
 
     public function parseWorkoutType($workoutText)
